6c0c3bb6df2ad4599df436a6b7c7467e24482beb
Afonso Fernandes <81321197+Gongas28@users.noreply.github.com>
2023-12-20 23:26:52 +0000

    Update day1.py

--- day1.py
+++ day1.py
@@ -2,19 +2,16 @@ def sum_calibration_values(calibration_document):
     total_sum = 0
 
     for line in calibration_document:
-        # Extract the first and last digits from each line
+
         first_digit = int(line[0])
         last_digit = int(line[-1])
 
-        # Combine the digits to form a two-digit number
         calibration_value = first_digit * 10 + last_digit
 
-        # Add the calibration value to the total sum
         total_sum += calibration_value
 
     return total_sum
 
-# Example calibration document
 calibration_document = [
     "1abc2",
     "pqr3stu8vwx",
@@ -22,8 +19,6 @@ calibration_document = [
     "treb7uchet"
 ]
 
-# Calculate the sum of calibration values
 result = sum_calibration_values(calibration_document)
 
-# Print the result
 print(result)
