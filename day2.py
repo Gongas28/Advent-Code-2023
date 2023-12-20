21643d02b0b9614dfd47de5ea406273531ad8b3e
Afonso Fernandes <81321197+Gongas28@users.noreply.github.com>
2023-12-20 23:29:41 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -29,7 +29,6 @@ def possible_games_sum(input_data):
     
     return sum(possible_games)
 
-# Your input data
 input_data = [
     "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
     "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
