f5b328c73705c9299792db6511c020eb0febd959
Afonso Fernandes <81321197+Gongas28@users.noreply.github.com>
2023-12-22 16:54:41 +0000

    Update day4.py

--- day4.py
+++ day4.py
@@ -11,7 +11,6 @@ def calculate_points(card_data):
 
     return total_points
 
-# Puzzle Input
 card_data = [
     "41 48 83 86 17 | 83 86  6 31 17  9 48 53",
     "13 32 20 16 61 | 61 30 68 82 17 32 24 19",
