3cb8945a5444182155a4823454b0edcecd4d664e
Afonso Fernandes <81321197+Gongas28@users.noreply.github.com>
2023-12-25 01:36:14 +0000

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,72 @@
+def convert(seed, seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature, temperature_to_humidity, humidity_to_location):
+    soil = seed
+    fertilizer = soil
+    water = fertilizer
+    light = water
+    temperature = light
+    humidity = temperature
+    location = humidity
+
+    # Convert seed to soil
+    for entry in seed_to_soil:
+        dest_range, src_range, length = entry
+        if seed in range(src_range, src_range + length):
+            soil = dest_range + (seed - src_range)
+
+    # Convert soil to fertilizer
+    for entry in soil_to_fertilizer:
+        dest_range, src_range, length = entry
+        if soil in range(src_range, src_range + length):
+            fertilizer = dest_range + (soil - src_range)
+
+    # Convert fertilizer to water
+    for entry in fertilizer_to_water:
+        dest_range, src_range, length = entry
+        if fertilizer in range(src_range, src_range + length):
+            water = dest_range + (fertilizer - src_range)
+
+    # Convert water to light
+    for entry in water_to_light:
+        dest_range, src_range, length = entry
+        if water in range(src_range, src_range + length):
+            light = dest_range + (water - src_range)
+
+    # Convert light to temperature
+    for entry in light_to_temperature:
+        dest_range, src_range, length = entry
+        if light in range(src_range, src_range + length):
+            temperature = dest_range + (light - src_range)
+
+    # Convert temperature to humidity
+    for entry in temperature_to_humidity:
+        dest_range, src_range, length = entry
+        if temperature in range(src_range, src_range + length):
+            humidity = dest_range + (temperature - src_range)
+
+    # Convert humidity to location
+    for entry in humidity_to_location:
+        dest_range, src_range, length = entry
+        if humidity in range(src_range, src_range + length):
+            location = dest_range + (humidity - src_range)
+
+    return location
+
+# Given maps
+seed_to_soil = [(50, 98, 2), (52, 50, 48)]
+soil_to_fertilizer = [(0, 15, 37), (37, 52, 2), (39, 0, 15)]
+fertilizer_to_water = [(49, 53, 8), (0, 11, 42), (42, 0, 7), (57, 7, 4)]
+water_to_light = [(88, 18, 7), (18, 25, 70)]
+light_to_temperature = [(45, 77, 23), (81, 45, 19), (68, 64, 13)]
+temperature_to_humidity = [(0, 69, 1), (1, 0, 69)]
+humidity_to_location = [(60, 56, 37), (56, 93, 4)]
+
+# Given seeds
+seeds = [79, 14, 55, 13]
+
+# Find the lowest location
+lowest_location = float('inf')
+for seed in seeds:
+    location = convert(seed, seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature, temperature_to_humidity, humidity_to_location)
+    lowest_location = min(lowest_location, location)
+
+print("O número de localização mais baixo é:", lowest_location)
