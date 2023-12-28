096cf68a921fae6ca1b14ea2df9be3ff42314616
Afonso Fernandes <81321197+Gongas28@users.noreply.github.com>
2023-12-28 14:34:22 +0000

    Create day24.java

---
+++ day24.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Hailstone {
+    int x, y, z, vx, vy, vz;
+
+    public Hailstone(int x, int y, int z, int vx, int vy, int vz) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.vx = vx;
+        this.vy = vy;
+        this.vz = vz;
+    }
+
+    public void move() {
+        x += vx;
+        y += vy;
+        z += vz;
+    }
+}
+
+public class HailstoneCollision {
+    public static void main(String[] args) {
+        // Create a list of hailstones
+        List<Hailstone> hailstones = new ArrayList<>();
+        hailstones.add(new Hailstone(19, 13, 30, -2, 1, -2));
+        hailstones.add(new Hailstone(18, 19, 22, -1, -1, -2));
+        hailstones.add(new Hailstone(20, 25, 34, -2, -2, -4));
+        hailstones.add(new Hailstone(12, 31, 28, -1, -2, -1));
+        hailstones.add(new Hailstone(20, 19, 15, 1, -5, -3));
+
+        // Set the test area boundaries
+        int minX = 7;
+        int maxX = 27;
+        int minY = 7;
+        int maxY = 27;
+
+        // Set the number of time steps to simulate
+        int timeSteps = 1000;
+
+        // Check for intersections
+        int intersections = 0;
+
+        for (int t = 0; t < timeSteps; t++) {
+            for (int i = 0; i < hailstones.size() - 1; i++) {
+                for (int j = i + 1; j < hailstones.size(); j++) {
+                    Hailstone hailstoneA = hailstones.get(i);
+                    Hailstone hailstoneB = hailstones.get(j);
+
+                    if (willIntersect(hailstoneA, hailstoneB, minX, maxX, minY, maxY)) {
+                        intersections++;
+                    }
+                }
+            }
+
+            // Move hailstones to the next time step
+            for (Hailstone hailstone : hailstones) {
+                hailstone.move();
+            }
+        }
+
+        System.out.println("Number of intersections: " + intersections);
+    }
+
+    private static boolean willIntersect(Hailstone a, Hailstone b, int minX, int maxX, int minY, int maxY) {
+        // Check if the paths of two hailstones will intersect within the specified test area
+        int ax = a.x;
+        int ay = a.y;
+        int bx = b.x;
+        int by = b.y;
+
+        return ax >= minX && ax <= maxX && ay >= minY && ay <= maxY &&
+               bx >= minX && bx <= maxX && by >= minY && by <= maxY;
+    }
+}
