58bd8ee7d0006110e58252c7e14caa7613b0deb3
Afonso Fernandes <81321197+Gongas28@users.noreply.github.com>
2023-12-28 14:36:27 +0000

    Create day9.py

---
+++ day9.py
@@ -0,0 +1,23 @@
+def extrapolate_oasis_report(oasis_report):
+    def get_next_sequence(sequence):
+        return [sequence[i] + sequence[i + 1] for i in range(len(sequence) - 1)]
+
+    extrapolated_values = []
+
+    for history in oasis_report:
+        while any(history):
+            history = get_next_sequence(history)
+
+        extrapolated_values.append(history[-1])
+
+    return sum(extrapolated_values)
+
+# Example usage
+oasis_report = [
+    [0, 3, 6, 9, 12, 15],
+    [1, 3, 6, 10, 15, 21],
+    [10, 13, 16, 21, 30, 45],
+]
+
+result = extrapolate_oasis_report(oasis_report)
+print(result)
