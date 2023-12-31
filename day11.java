17b78be4bc744681752ec315bd7d13837c41ce49
Afonso Fernandes <81321197+Gongas28@users.noreply.github.com>
2023-12-31 17:26:58 +0000

    Update day11.java

--- day11.java
+++ day11.java
@@ -21,13 +21,11 @@ public class GalaxyShortestPath {
     }
 
     public static int sumOfShortestPaths(String[] input) {
-        // Parse input to create a 2D array representing the expanded universe
         char[][] universe = new char[input.length][input[0].length()];
         for (int i = 0; i < input.length; i++) {
             universe[i] = input[i].toCharArray();
         }
 
-        // Identify galaxies and assign unique numbers to them
         Map<Character, Integer> galaxyNumbers = new HashMap<>();
         int galaxyCount = 0;
         for (int i = 0; i < universe.length; i++) {
@@ -38,7 +36,6 @@ public class GalaxyShortestPath {
             }
         }
 
-        // Calculate the shortest path between every pair of galaxies
         int sumOfShortestPaths = 0;
         for (char galaxy1 : galaxyNumbers.keySet()) {
             for (char galaxy2 : galaxyNumbers.keySet()) {
@@ -50,14 +47,5 @@ public class GalaxyShortestPath {
         }
 
         return sumOfShortestPaths;
-    }
-
-    public static int shortestPath(char[][] universe, int startGalaxy, int endGalaxy) {
-        // Implement your shortest path algorithm here (e.g., BFS)
-        // This is a placeholder, and you may need to customize it based on your specific needs.
-        // The algorithm should consider only steps that move up, down, left, or right exactly one . or # at a time.
-        // You may want to use a BFS algorithm to find the shortest path.
-        // Return the length of the shortest path.
-        return 0;
     }
 }
